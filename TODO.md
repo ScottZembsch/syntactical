3d31fa56ca7a4cb95dd37c52dfdc49acee54f82c
Scott Zembsch <scottzembsch@gmail.com>
2023-12-19 12:16:39 -0800

    chipping

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 
 fix input feild
 create gamereacap popup with stats after game ends
-make new game button functional 
 
 
 create server 
---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const PORT = 5000;
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
+})
